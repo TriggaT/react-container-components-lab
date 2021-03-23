d06a3f244c421b1514b19c815ffbefb4597b6579
Trayshawn <traytwebb14@yahoo.com>
2021-03-23 17:47:15 -0500

    commit tree dbaead8190186fb975055a70f55c8c4b893e6d40

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,45 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor(){
+        super()
+        this.state = {
+            reviews: [],
+            searchTerm: ""
+        }
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+        fetch(URL + `&query=${this.state.searchTerm}`)
+        .then(r => r.json())
+        .then(data => this.setState({
+            reviews: data.results
+        }))
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            searchTerm: e.target.value
+        })
+    }
+
+
+    render(){
+        return(
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.handleSubmit}>
+                <input type="text" onChange={this.handleChange}></input>
+                <button type="submit">Search Reviews</button>
+                </form>
+                <h1>Seached Movie Reviews</h1>
+                <MovieReviews reviews={this.state.reviews} />
+
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer
+
